71a1da80a9473dbb52dcfe7ee432a107eb8a425a
oriol-valentines <oriolvalentinesmiro@gmail.com>
2024-11-14 18:43:02 +0100

    Commit 3
    Se han implementado canvios los cuales necesitan arreglarse

--- src/Juego_Colgado/El_Juego_del_Colgado.java
+++ src/Juego_Colgado/El_Juego_del_Colgado.java
@@ -1,6 +1,7 @@
 package Juego_Colgado;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class El_Juego_del_Colgado {
 
@@ -9,9 +10,15 @@ public class El_Juego_del_Colgado {
 		
 		
 		//VARIABLES PARA EL JUEGO
-		int jugadores = 0;
+		int jugadores = 2;
 		int rondas = 0;
-		int partida = 0;
+		String[] palabras = new String[4];
+		palabras[0] = "hola";
+		palabras[1] = "lindo";
+		palabras[2] = "gustar";
+		palabras[3] = "pantalla";
+		palabras[4] = "morder";
+		String partida;
 		
 		//ENTRADA DE INFO 
 		Scanner x = new Scanner(System.in);
@@ -23,11 +30,24 @@ public class El_Juego_del_Colgado {
 		System.out.println("|_____________________|");
 		
 		
-		//DEFINIR LAS RONDAS
+		//DEFINIR LAS RONDAS Y JUGADORES
 		System.out.print("¿Cuantas rondas quiere jugar? ");
 		rondas = x.nextInt();
-		for(int ronda = 2; ronda <= rondas; ronda++) {
+		if(rondas == 1) {
+			System.out.println("No se puede jugar solamente 1 ronda, como minimo deves jugar 2 rondas");
+			return;
+		}
+		System.out.print("¿Cuantos Jugadores? ");
+		jugadores = x.nextInt();
+		for(int ronda = 1; ronda <= rondas; ronda++) {
 			System.out.println("Ronda " + ronda);
+			for(int jugador = 1; jugador <= jugadores; jugador++) {
+				System.out.println("Jugador " + jugador);
+				
+				System.out.print("Inserta: ");
+				jugador = x.nextInt();
+				
+			}
 		}
 		
 		
