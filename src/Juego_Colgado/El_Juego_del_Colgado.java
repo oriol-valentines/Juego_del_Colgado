9c2b60552188770ef2c30d9e2572693a346b191e
oriol-valentines <oriolvalentinesmiro@gmail.com>
2024-11-19 17:16:35 +0100

    Commit 5
    Se ha mejorado todo, se han añadido mas funciones como añadir '_' en los espacios donde no se haya acertado la letra entre otras...

--- src/Juego_Colgado/El_Juego_del_Colgado.java
+++ src/Juego_Colgado/El_Juego_del_Colgado.java
@@ -1,7 +1,7 @@
 package Juego_Colgado;
 
 import java.util.Scanner;
-import java.util.Random;
+import java.util.Arrays;
 
 public class El_Juego_del_Colgado {
 
@@ -10,45 +10,82 @@ public class El_Juego_del_Colgado {
 		
 		
 		//VARIABLES PARA EL JUEGO
-		int jugadores = 2;
-		int rondas = 0;
-		String[] palabras = new String[4];
-		palabras[0] = "hola";
-		palabras[1] = "lindo";
-		palabras[2] = "gustar";
-		palabras[3] = "pantalla";
-		palabras[4] = "morder";
 		String partida;
-		
+		String palabras;
+		String[] palabrasS;
+		boolean palabraEncontrada = false;
+				
 		//ENTRADA DE INFO 
 		Scanner x = new Scanner(System.in);
-		
+				
 		//PRIMERA PANTALLA QUE MOSTRARA
 		System.out.println(" _____________________");
 		System.out.println("|                     |");
 		System.out.println("| JUEGO DEL COLGADO!! |");
 		System.out.println("|_____________________|");
+				
+		//INTRODUCIR LA PALABRA SECRETA
+		System.out.print("Introduce la palabra secreta: " );
+		palabras = x.nextLine().toLowerCase();
+		palabrasS = palabras.split(" "); //EL SPLIT SEPARA LOS CARACTERES PARA PODER GUARDARLOS 
+		// palabrasS = palabras.toCharArray(); ESTA ES OTRA FORMA PARA INSERTAR LA PALABRA EN EL ARRAY VACIO
 		
-		
-		//DEFINIR LAS RONDAS Y JUGADORES
-		System.out.print("¿Cuantas rondas quiere jugar? ");
-		rondas = x.nextInt();
-		if(rondas == 1) {
-			System.out.println("No se puede jugar solamente 1 ronda, como minimo deves jugar 2 rondas");
-			return;
+		/* for (int i = 0; i<palabrasS.length; i++) { //PRUEBA PARA VER SI MUESTRA LA PALABRA SECRETA
+			System.out.print(palabrasS[i]);
 		}
-		System.out.print("¿Cuantos Jugadores? ");
-		jugadores = x.nextInt();
-		for(int ronda = 1; ronda <= rondas; ronda++) {
-			System.out.println("Ronda " + ronda);
-			for(int jugador = 1; jugador <= jugadores; jugador++) {
-				System.out.println("Jugador " + jugador);
-				
-				System.out.print("Inserta: ");
-				jugador = x.nextInt();
-				
+		*/
+		
+		
+		//DEFINIR LOS JUGADORES
+		System.out.print("¿Cuantos jugadores quieren jugar? ");
+		int jugadoresNum = x.nextInt();
+		
+		int[] jugadores;
+		jugadores = new int[jugadoresNum];
+		
+		int[] vidas;
+		vidas = new int[jugadores.length];
+		for (int i = 0; i < vidas.length; i++) {
+			vidas[i] = 6;
+		}
+		
+		//AÑADIR EL CARACTER '_' EN LOS ESPACIOS QUE NO HAYA NINGUN CARACTER AUN.
+		char[] progreso = new char[palabras.length()];
+		Arrays.fill(progreso, '_'); //ARRAYS.FILL(CARACTER1, CARACTER2) ES PARA RELLENAR
+		
+		while(true) {
+			
+			for (int i = 0; i < jugadores.length; i++) {
+				System.out.println("Jugador " + jugadores[i]);
 			}
+			
+			System.out.print("Inserta la letra que creas que va a aparecer en el texto: ");
+			char letra = x.next().toLowerCase().charAt(0);
+			
+			for (int i = 0; i < palabrasS.length; i++) {
+				if(palabrasS[i].charAt(i) == letra) {
+					progreso[i] = letra;
+					palabraEncontrada = true;
+				}
+			}System.out.print("");
+			
 		}
+		
+		
+			
+			
+					
+					
+					
+					
+		
+		
+		
+		
+		//if (Array.equals(nombre_array1, nombre_Array2)){
+		// if (nombre_string == Array.toString(nombre_array)){
+		
+		
 		
 		
 		
