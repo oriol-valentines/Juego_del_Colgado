95d5a5fd83c2c54a1a64912a99e5de20495834c3
oriol-valentines <oriolvalentinesmiro@gmail.com>
2024-11-22 17:59:21 +0100

    Commit 9
    Se han arreglado los errores, también se ha implementado la función de que si son menos de dos jugadores, nos va a mostrar una pantalla la cual nos va a decir que no pueden ser menos de dos y nos va a pedir que insertemos los jugadores de nuevo, también se ha cambiado la estructura y he implementado una mejora grafica

--- src/Juego_Colgado/El_Juego_del_Colgado.java
+++ src/Juego_Colgado/El_Juego_del_Colgado.java
@@ -36,6 +36,12 @@ public class El_Juego_del_Colgado {
 		System.out.print("¿Cuantos jugadores quieren jugar? ");
 		int jugadoresNum = x.nextInt();
 		
+		if(jugadoresNum <= 1) {
+			System.out.println("NO PUEDEN JUGAR MENOS DE DOS PERSONAS!!");
+			System.out.print("¿Cuantos jugadores quieren jugar (MINIMO 2)? ");
+			jugadoresNum = x.nextInt();
+		}
+		
 		int[] jugadores;
 		jugadores = new int[jugadoresNum];
 		
@@ -47,24 +53,23 @@ public class El_Juego_del_Colgado {
 		
 		//AÑADIR EL CARACTER '_' EN LOS ESPACIOS QUE NO HAYA NINGUN CARACTER AUN.
 		char[] progreso = new char[palabras.length()];
-		Arrays.fill(progreso, '_'); //ARRAYS.FILL(CARACTER1, CARACTER2) ES PARA RELLENAR
+		Arrays.fill(progreso, '-'); //ARRAYS.FILL(CARACTER1, CARACTER2) ES PARA RELLENAR
 		
 		while(true) {
-			
 			for (int i = 0; i < jugadores.length; i++) {
-				System.out.println("__________________________");
+				System.out.println("_______________________________________________________");
 				System.out.println(" ");
 				System.out.println("Jugador: " + (i + 1));
-				System.out.println("Vidas Restantes: " + vidas[i]);
+				System.out.println("Vidas Restantes " +vidas[i]);
 				
 				for (int j = 0; j < progreso.length; j++) {
-					System.out.print(progreso[i]);
-				}System.out.print(" ");
+					System.out.print(progreso[j]);
+				}System.out.println(" ");
 				
-				System.out.print("Inserta la letra que creas correcta: ");
+				System.out.print("Inserta la letra que creas que va a aparecer en el texto: ");
 				char letra = x.next().toLowerCase().charAt(0);
 				
-				boolean acierto = false;
+				boolean acierto = false; 
 				for (int j = 0; j < palabrasS.length; j++) {
 					if(palabrasS[j] == letra) {
 						progreso[j] = letra;
@@ -73,22 +78,34 @@ public class El_Juego_del_Colgado {
 				}
 				
 				if(acierto) {
-					System.out.println("---------------------------------");
-					System.out.println("Has acertado la letra, MUY BIEN!!");
+					System.out.println("---------------------------");
+					System.out.println("Has acertado la letra!! ");
 					System.out.println("La letra acertada es: " + letra);
-					System.out.println("---------------------------------");
+					System.out.println("---------------------------");
 				}else {
 					System.out.println("No has acertado la letra!!");
 					vidas[i]--;
 				}
 				
+				if (Arrays.equals(palabrasS, progreso)){
+					System.out.println(" ___________________________________________________");
+					System.out.println("|                                                   |");
+					System.out.println("           Has acertado la palabra Jugador " + (i + 1));
+					System.out.println("           la palabra acertada es: " + palabras.toUpperCase());
+					System.out.println("|___________________________________________________|");
+					return;
+				}
+				
 				if(vidas[i] <= 0) {
-					System.out.println("Te has quedado sin vidas, HAS PERDIDO!!");
-					System.out.println("SE CIERRA EL JUEGO...");
+					System.out.println(" ___________________________________________________");
+					System.out.println("|                                                   |");
+					System.out.println("|      te has quedado sin vidas, Has perdido!!!     |");
+					System.out.println("|               Se cierra el juego...               |");
+					System.out.println("|___________________________________________________|");
+					return;
 				}
 				
 				
-			
 			}
 			
 		}
